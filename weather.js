4d98b962d27c29d31498837fedec31917790e540
washios <aigorudogyarakushi2@gmail.com>
2026-05-30 16:42:58 +0900

    Update weather.js

--- weather.js
+++ weather.js
@@ -38,3 +38,59 @@ function fakeWeather(){
 }
 
 fakeWeather();
+
+function createRain(){
+
+  setInterval(()=>{
+
+    const rain =
+    document.createElement("div");
+
+    rain.className =
+    "rain";
+
+    rain.style.left =
+    Math.random()*100+"vw";
+
+    rain.style.animationDuration =
+    .5+Math.random()+"s";
+
+    document.body.appendChild(
+      rain
+    );
+
+    setTimeout(()=>{
+      rain.remove();
+    },2000);
+
+  },50);
+
+}
+
+function createSnow(){
+
+  setInterval(()=>{
+
+    const snow =
+    document.createElement("div");
+
+    snow.className =
+    "snow";
+
+    snow.style.left =
+    Math.random()*100+"vw";
+
+    snow.style.animationDuration =
+    3+Math.random()*4+"s";
+
+    document.body.appendChild(
+      snow
+    );
+
+    setTimeout(()=>{
+      snow.remove();
+    },8000);
+
+  },150);
+
+}
