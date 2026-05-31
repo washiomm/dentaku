c0e06c47828c4f5989e3bd29d8376748f86fd26f
washios <aigorudogyarakushi2@gmail.com>
2026-05-31 18:42:41 +0900

    Add calculator JS

---
+++ calculator/script.js
@@ -0,0 +1,1 @@
+const display=document.getElementById('display');function append(v){display.value+=v}function clearDisplay(){display.value=''}function backspace(){display.value=display.value.slice(0,-1)}function calculate(){try{display.value=eval(display.value)}catch{display.value='Error'}}
